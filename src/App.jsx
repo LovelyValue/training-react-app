c6f6b5882a2119142c138059d0bbbe9bb7a021c0
Lovely.Value <aid.l.v.esd@gmail.com>
2024-11-11 18:58:08 +0300

    Композиция

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Button from './components/Button/Button';
+import CardButton from './components/CardButton/CardButton';
 import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
@@ -19,11 +20,20 @@ function App() {
 
 	return (
 		<>
-			<JournalItem
-				title={data[0].title}
-				text={data[0].text}
-				date={data[0].date}
-			/>
+			<CardButton>
+				<JournalItem
+					title={data[0].title}
+					text={data[0].text}
+					date={data[0].date}
+				/>
+			</CardButton>
+			<CardButton>
+				<JournalItem
+					title={data[1].title}
+					text={data[1].text}
+					date={data[1].date}
+				/>
+			</CardButton>
 			<Button />
 		</>
 	);
---
+++ src/components/CardButton/CardButton.jsx
@@ -0,0 +1,7 @@
+import './CardButton.css';
+
+function CardButton({ children }) {
+	return <button className='card-button'>1{children}</button>;
+}
+
+export default CardButton;

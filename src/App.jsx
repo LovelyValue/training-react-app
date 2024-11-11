322eb5a0837d33ee64d87338588acdfff3f732d1
Lovely.Value <aid.l.v.esd@gmail.com>
2024-11-11 17:43:09 +0300

    Пропсы и красивая дата

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,27 @@ import Button from './components/Button/Button';
 import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
+	const data = [
+		{
+			title: 'Movie',
+			text: 'text',
+			date: new Date(),
+		},
+
+		{
+			title: 'Books',
+			text: 'sex',
+			date: new Date(),
+		},
+	];
+
 	return (
 		<>
-			<JournalItem />
+			<JournalItem
+				title={data[0].title}
+				text={data[0].text}
+				date={data[0].date}
+			/>
 			<Button />
 		</>
 	);
--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,17 +1,16 @@
 import './JournalItem.css';
 
-function JournalItem() {
+function JournalItem({ title, text, date }) {
+	const formatedDate = Intl.DateTimeFormat('ru-Ru').format(date);
+
 	return (
-		<>
-			<h2 className='journal-item__header'>Lorem ipsum dolor sit amet.</h2>
+		<div className='journal-item'>
+			<h2 className='journal-item__header'>{title}</h2>
 			<h2 className='journal-item__body'>
-				<div className='journal-item__date'>24.13.2023</div>
-				<div className='journal-item__text'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
-					sunt.
-				</div>
+				<div className='journal-item__date'>{formatedDate}</div>
+				<div className='journal-item__text'>{text}</div>
 			</h2>
-		</>
+		</div>
 	);
 }
 

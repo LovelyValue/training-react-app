b30a14e9c55456a28b3d66890e6c9f97e6331b79
Lovely.Value <aid.l.v.esd@gmail.com>
2024-11-19 17:58:08 +0300

    работа с SVG

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import './Header.css';
 
 function Header() {
-	return <div className='header'>Logo</div>;
+	return <img className='logo' src='/logo.svg' alt='Логотип Журнала' />;
 }
 
 export default Header;
--- src/components/JournalAddButton/JournalAddButton.jsx
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -2,7 +2,12 @@ import CardButton from '../CardButton/CardButton';
 import './JournalAddButton.css';
 
 function JournalAddButton() {
-	return <CardButton className='journal-add'>Новое сообщение</CardButton>;
+	return (
+		<CardButton className='journal-add'>
+			<img src='/add.svg' alt='add' />
+			Новое сообщение
+		</CardButton>
+	);
 }
 
 export default JournalAddButton;

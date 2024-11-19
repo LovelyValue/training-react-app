e4a3a668e9ff10bb912b2391394f548ea41cbc54
Lovely.Value <aid.l.v.esd@gmail.com>
2024-11-19 17:23:55 +0300

    Вёрстка layout and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css';
-import Button from './components/Button/Button';
 import CardButton from './components/CardButton/CardButton';
+import Header from './components/Header/Header';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalItem from './components/JournalItem/JournalItem';
+import JournalList from './components/JournalList/JournalList';
+import Body from './layouts/Body/Body';
+import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
 function App() {
 	const data = [
@@ -19,23 +23,30 @@ function App() {
 	];
 
 	return (
-		<>
-			<CardButton>
-				<JournalItem
-					title={data[0].title}
-					text={data[0].text}
-					date={data[0].date}
-				/>
-			</CardButton>
-			<CardButton>
-				<JournalItem
-					title={data[1].title}
-					text={data[1].text}
-					date={data[1].date}
-				/>
-			</CardButton>
-			<Button />
-		</>
+		<div className='app'>
+			<LeftPanel>
+				<Header />
+				<JournalAddButton></JournalAddButton>
+				<JournalList>
+					<CardButton>
+						<JournalItem
+							title={data[0].title}
+							text={data[0].text}
+							date={data[0].date}
+						/>
+					</CardButton>
+					<CardButton>
+						<JournalItem
+							title={data[1].title}
+							text={data[1].text}
+							date={data[1].date}
+						/>
+					</CardButton>
+				</JournalList>
+			</LeftPanel>
+
+			<Body>body</Body>
+		</div>
 	);
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import './Header.css';
+
+function Header() {
+	return <div className='header'>Logo</div>;
+}
+
+export default Header;

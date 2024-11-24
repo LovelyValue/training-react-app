d4757e35e140f0ec2f59cde501b9a539affea1df
Lovely.Value <aid.l.v.esd@gmail.com>
2024-11-24 19:51:43 +0300

    Отображение по условию

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import { useState } from 'react';
 import './App.css';
-import CardButton from './components/CardButton/CardButton';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
-import JournalItem from './components/JournalItem/JournalItem';
+
 import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
 const INITIAL_DATA = [
-	{
-		id: 1,
-		title: 'Movie',
-		text: 'text',
-		date: new Date(),
-	},
-
-	{
-		id: 2,
-		title: 'Books',
-		text: 'sex',
-		date: new Date(),
-	},
+	// {
+	// 	id: 1,
+	// 	title: 'Movie',
+	// 	text: 'text',
+	// 	date: new Date(),
+	// },
+	// {
+	// 	id: 2,
+	// 	title: 'Books',
+	// 	text: 'sex',
+	// 	date: new Date(),
+	// },
 ];
 
 function App() {
@@ -40,26 +38,12 @@ function App() {
 		]);
 	};
 
-	const sortItems = (a, b) => {
-		if (a.date < b.date) {
-			return 1;
-		} else {
-			return -1;
-		}
-	};
-
 	return (
 		<div className='app'>
 			<LeftPanel>
 				<Header />
 				<JournalAddButton></JournalAddButton>
-				<JournalList>
-					{items.sort(sortItems).map(el => (
-						<CardButton key={el.id}>
-							<JournalItem title={el.title} text={el.text} date={el.date} />
-						</CardButton>
-					))}
-				</JournalList>
+				<JournalList items={items} />
 			</LeftPanel>
 
 			<Body>

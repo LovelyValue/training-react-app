4ea074cfb914ab9495f889465cb850581790b28c
Lovely.Value <aid.l.v.esd@gmail.com>
2024-11-24 17:42:26 +0300

    работа со списком данных

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -23,7 +23,12 @@ function JournalForm() {
 			<input type='date' name='date' />
 			<input type='text' name='tag' value={inputData} onChange={inputChange} />
 			<textarea name='post' cols={30} rows={10}></textarea>
-			<Button text='Сохранить' />
+			<Button
+				text='Сохранить'
+				onClick={() => {
+					console.log('Clicked');
+				}}
+			/>
 		</form>
 	);
 }
